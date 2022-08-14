63fa0896112c98fdd98b8d667989b608454762b6
Berkay Yelken <berkayelken@gmail.com>
2022-08-14 19:29:16 +0300

    Objective C converter service is created

---
+++ ms_objectivec_converter/src/test/java/io/github/berkayelken/objectivec/converter/MsObjectivecConverterApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.berkayelken.objectivec.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsObjectivecConverterApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
